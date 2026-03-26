b54e5df5fabc60655be596e4b4996e09dddc0ecc
Jesus <jiesusdance@gmail.com>
2026-03-26 11:53:35 +0200

    Added docstring

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+"""
+Flask PDF-merger-tool.
+
+Description
+
+This module implements a single web application pdf merger using Bootstrap styling.
+Features includes:
+- pypdf for merging files
+- pathlib for easy access to path of files
+- Jinja2 templates
+- flash messages for feedback
+
+Routes:
+
+- '/': main page with form
+- '/clear': clear list of pdf before merging
+- '/merge': merging pdf files
+- '/delete/<name>': delete file by name
+"""
+
 from pathlib import Path
 from pypdf import PdfWriter, PdfReader
 from flask import Flask, render_template, request, flash, url_for, redirect, send_file
